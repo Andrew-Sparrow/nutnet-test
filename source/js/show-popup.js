851b61f2f06cfc3ddbbd0ce3be58a8ad6328b90e
Andrew <8911102@gmail.com>
2021-06-21 08:34:23 +0500

    popup was corrected for scroll

--- source/js/show-popup.js
+++ source/js/show-popup.js
@@ -30,11 +30,12 @@ window.addEventListener("scroll", function () {
 
 // ==== Variables for popup
 var modal = document.querySelector(".popup"),
-  close = document.querySelector(".close-modal"),
+  close = document.querySelector(".popup__close"),
   root = document.getElementsByTagName("html")[0];
 
 // Do stuff when player is ready
 function onPlayerReady(event) {
+  window.scrollY = 1000;
   document.querySelector(".watch__play").addEventListener("click", function () {
     modal.classList.add("popup__show");
     // Play video
@@ -45,33 +46,29 @@ function onPlayerReady(event) {
 
   function closePopup() {
     modal.classList.remove("popup__show");
-    root.classList.remove("no-scroll");
-    window.scrollY = scrollPosition;
+    window.scrollTo(0, 1000);
 
     event.target.pauseVideo();
   }
 
   // Close if outside box is clicked
-  window.addEventListener("click", function (e) {
-    if (e.target === modal) {
+  window.addEventListener("click", function (evt) {
+    if (evt.target === modal) {
       closePopup();
     }
   });
 
-  window.document.onkeydown = function (e) {
-    if (!e) {
-      e = event;
+  window.document.onkeydown = function (evt) {
+    if (!evt) {
+      evt = event;
     }
-    if (e.key === "Escape") {
+    if (evt.key === "Escape") {
       closePopup();
     }
   }
 
   // Close if close button is clicked
   close.addEventListener("click", function () {
-    modal.classList.remove("popup__show");
-    root.classList.remove("no-scroll");
-    window.scrollY = scrollPosition;
-    event.target.pauseVideo();
+    closePopup();
   });
 }
